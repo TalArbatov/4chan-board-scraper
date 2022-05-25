6218003620bfc038c04a37080ae6250503dca209
= <talarbatov98@gmail.com>
2022-05-25 21:02:23 +0000

    added wget support

--- src/generate_threads.ts
+++ src/generate_threads.ts
@@ -2,6 +2,9 @@ import puppeteer from 'puppeteer';
 import path from 'path';
 import { ThreadWatcher } from './ThreadWatcher';
 import fs from 'fs/promises';
+import shell from 'shelljs';
+import { Post } from './types';
+
 const twPath = path.join(__dirname, '..', 'data', 'thread_watcher.json');
 
 
@@ -28,19 +31,41 @@ const options = {
 			const nodeList = document.querySelectorAll('.postContainer');
 
 			for (const elem of nodeList) {
-				posts.push({
+				const post: Post = {
 					id: elem.id,
 					time: document.querySelector(`#${elem.id} .dateTime`)!.textContent!.split(' ')[0],
-					image: {
-						name: document.querySelector(`#${elem.id} .fileText > a`)?.textContent,
-						url: document.querySelector(`#${elem.id} .fileText > a`)?.getAttribute('href'),
-					},
-						text: document.querySelector(`#${elem.id} .postMessage`)?.textContent,
-				});
+						text: document.querySelector(`#${elem.id} .postMessage`)?.textContent as string,
+				};
+
+		const imageName = document.querySelector(`#${elem.id} .fileText > a`)?.textContent;
+
+				if (imageName) {
+							post.image = {
+							name: document.querySelector(`#${elem.id} .fileText > a`)?.textContent as string,
+							url: document.querySelector(`#${elem.id} .fileText > a`)?.getAttribute('href')?.replace('//', '') as string,
+						}
+				}
+				posts.push(post);
+
 			}
 
 			return posts;
 		});	
+
+		// download images and store local url
+		for (const post of posts) {
+			if (post.image) {
+				try {
+					const imagePath = path.resolve(__dirname, '..', 'data', 'images', post.image.name);
+					shell.exec(`wget -O ${imagePath} ${post.image.url}`);
+					post.image.url = imagePath;
+				} catch(e) {
+					console.log(e);
+				}
+			}
+		}
+
+		// save thread JSON
 
 		const threadPath = path.resolve(__dirname, '..', 'data', 'threads', `${thread.id}.json`);
 		await fs.writeFile(threadPath, JSON.stringify(posts));
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+type Post = {
+	id: string;
+	time: string;
+	text: string;
+	image?: {
+		name: string;
+		url: string;
+	}		
+}
+
+
+export { Post }
